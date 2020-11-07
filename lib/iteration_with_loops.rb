ed6458c1a19f9a361b55012ac86e5b0e90b38aa8
Cameron Bisca <cjbisca@gmail.com>
2020-11-07 00:58:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,15 +5,13 @@ def find_min_in_nested_arrays(src)
   
   while row_index < src.length do 
     element_index = 0 
-    daily_lows = []
     
     while element_index < src[row_index].length do 
       coldest_temp =  src[row_index].sort
-      daily_lows.push(coldest_temp[0])
+      lows_for_week << coldest_temp[0]
       element_index += 25 
     end 
     
-    lows_for_week << daily_lows 
     row_index += 1 
   end    
   lows_for_week 
